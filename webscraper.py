21adaf9bca499c438dd37fd4d13fb5404913ea68
diyanr <diyan.rahaman@verizon.net>
2020-06-16 14:08:29 -0400

    Converted results to dataframe

--- webscraper.py
+++ webscraper.py
@@ -1,8 +1,8 @@
 import time
 import requests
+import pandas as pd
 from config import Config
 from forms import SearchForm
-
 
 from bs4 import BeautifulSoup
 from flask import Flask
@@ -18,8 +18,8 @@ def search():
     if form.validate_on_submit():
         target_url = form.website.data
         rowcount = int(form.records.data)
-        co_name, co_purpose = getCompany(target_url, rowcount)
-        return render_template('index.html', form=form, count=rowcount, company=co_name, purpose=co_purpose)
+        co_info = getCompany(target_url, rowcount)
+        return render_template('index.html', form=form, company=co_info)
     return render_template('index.html', form=form)
 
 
@@ -33,7 +33,7 @@ def getCompany(target_url, count):
         name.append(d["Name"])
         purpose.append(d["Purpose"])
         time.sleep(1)
-    return name, purpose
+    return pd.DataFrame({"name": name, "purpose": purpose})
 
 
 if __name__ == "__main__":
