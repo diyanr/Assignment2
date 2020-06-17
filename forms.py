a68d65a2e337b4fe1f9011b4fcbb9856fb31731e
diyanr <diyan.rahaman@verizon.net>
2020-06-17 00:29:55 -0400

    Added result form

--- forms.py
+++ forms.py
@@ -7,3 +7,7 @@ class SearchForm(FlaskForm):
     website = StringField('Website', validators=[DataRequired()])
     records = StringField('Records', validators=[DataRequired()])
     submit = SubmitField('Get Data')
+
+
+class ResultForm(FlaskForm):
+    submit = SubmitField('Download Data')
--- webscraper.py
+++ webscraper.py
@@ -6,7 +6,7 @@ from forms import SearchForm
 
 from bs4 import BeautifulSoup
 from flask import Flask
-from flask import render_template, flash
+from flask import render_template, Response
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,13 +14,19 @@ app.config.from_object(Config)
 
 @app.route('/index', methods=['GET', 'POST'])
 def search():
-    form = SearchForm()
-    if form.validate_on_submit():
-        target_url = form.website.data
-        rowcount = int(form.records.data)
+    s_form = SearchForm()
+    r_form = ResultForm()
+    co_info = pd.DataFrame()
+    if s_form.validate_on_submit():
+        target_url = s_form.website.data
+        rowcount = int(s_form.records.data)
         co_info = getCompany(target_url, rowcount)
-        return render_template('index.html', form=form, company=co_info)
-    return render_template('index.html', form=form)
+        return render_template('index.html', form1=s_form, form2=r_form, company=co_info)
+    elif r_form.validate_on_submit():
+        csv_data = co_info.to_csv(index=False)
+        return Response(csv_data, mimetype="text/csv",
+                        headers={"Content-disposition": "attachment; filename=mydata.csv"})
+    return render_template('index.html', form1=s_form)
 
 
 def getCompany(target_url, count):
